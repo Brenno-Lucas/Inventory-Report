a4e956582a2e81b00fc39f49c566821241bad2e3
Brenno-Lucas <brennolucas8@gmail.com>
2023-04-11 06:50:28 -0300

    Requisito 03

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,19 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data):
+        simple_report = SimpleReport.generate(data)
+
+        company_products = Counter(
+            [product["nome_da_empresa"] for product in data]
+        ).most_common()
+
+        simple_report += '\nProdutos estocados por empresa:\n'
+
+        for company, quantity in company_products:
+            simple_report += f"- {company}: {quantity}\n"
+
+        return simple_report
